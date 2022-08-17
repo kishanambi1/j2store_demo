48b21e6985dfef92b317da379e2d4b40eb7491c6
kishan <kishanambi1991@gmail.com>
2022-08-17 12:34:02 +0530

    j2store_demo project...

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 REM pytest -m "sanity" Test_Cases/ -rA --html=./Reports/report.html
 
-pytest -m "regression" Test_Cases/ -rA --html=./Reports/report.html
+REM pytest -m "regression" Test_Cases/ -rA --html=./Reports/report.html
 
-REM pytest -m "sanity or regression" Test_Cases/ -rA --html=./Reports/report.html
+pytest -m "sanity or regression" Test_Cases/ -rA --html=./Reports/report.html
 
 REM pytest -m "sanity and regression" Test_Cases/ -rA --html=./Reports/report.html
 
